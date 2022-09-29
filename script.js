c504eb2626fc55f1840ad51bba0a594846f008e2
Felipe Benini <112193194+FelBenini@users.noreply.github.com>
2022-09-29 14:34:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ $(document).ready(function () {
         $("#generate_qrcode").on("click", function () {
         let size = $("#size").val();
         let url = $("#URL").val();
-
-        let img = '<img src="https://chart.googleapis.com/chart?chs='+size+'x'+size+'&cht=qr&chl='+url+'" alt="qr code image">';
         
-        $("#qr_code").html(img);
+        $("#qr_code").html('<img src="https://chart.googleapis.com/chart?chs='+size+'x'+size+'&cht=qr&chl='+url+'" alt="qr code image">');
     });
 });
